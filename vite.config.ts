1de5105f5cebc51f653de0a9332f99e356c0a32e
RemontOK <remontok@example.com>
2025-10-09 21:43:12 +0500

    fix: добавлен base path для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CRM/',
   server: {
     port: 4000,
     open: true,
